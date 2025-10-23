619d145893e5da4d730c88a5847684bee6b799d7
rupanmentor <rupanguvimentor@gmail.com>
2025-10-23 19:14:43 +0530

    prep

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import Products from './Components/Products';
+import React from "react";
+import Products from "./Components/Products";
 import phone from "./assets/iphone.png";
 import lap from "./assets/laptop.png";
 import tab from "./assets/tab.png";
 
 const App = () => {
-  
+  let a = 10;
+  let arr = [1, 2, 3, 4, 5];
+  let obj = { batch: "FSD-WD-B-33", mode: "Weekday", mentor: "Rupan" };
+  let arrObj = [
+    {
+      name: "Iphone",
+      price: "$899",
+      desc: "This is Iphone 13 pro",
+      image: phone,
+    },
+    {
+      name: "Laptop",
+      price: "$1000",
+      desc: "This is Azsus Laptop",
+      image: lap,
+    },
+    {
+      name: "Tab",
+      price: "$499",
+      desc: "This is Ipad Pro",
+      image: tab,
+    },
+  ];
+
   return (
-    <div>
-       <Products name="Iphone" price="$899" desc="This is Iphone 13 pro" image={phone}/>
+    <>
+      {/* <Products name="Iphone" price="$899" desc="This is Iphone 13 pro" image={phone}/>
         <Products name="Laptop" price="$1000" desc="This is Azus Laptop" image={lap}/>
-         <Products name="Tab" price="$499" desc="This is Ipad pro" image={tab}/>
-    </div>
+         <Products name="Tab" price="$499" desc="This is Ipad pro" image={tab}/> */}
+    </>
   );
 };
 
